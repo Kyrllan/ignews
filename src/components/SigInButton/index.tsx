7b40a8c2074e4cc8be194e0caa490824d043f09f
Kyrllan Nogueira <kyrllan@hotmail.com>
2023-08-16 18:09:58 -0300

    session created

--- src/components/SigInButton/index.tsx
+++ src/components/SigInButton/index.tsx
@@ -1,13 +1,16 @@
 import {FaGithub} from 'react-icons/fa';
 import {FiX} from 'react-icons/fi';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
 
-  return isUserLoggedIn ? (
+  const { data: session } = useSession()
+
+  console.log(session)
+
+   return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
       Diego Fernandes
@@ -18,5 +21,5 @@ export function SignInButton() {
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
-  );
+  ); 
 }
