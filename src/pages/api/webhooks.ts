11c7987f9e4ae1a4a71b5c1dbdfc82bce68c4334
Kyrllan Nogueira <kyrllan@hotmail.com>
2023-08-28 21:55:29 -0300

    .

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,4 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+
+async function buffer(readable:Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === "string" ? Buffer.from(chunk) : chunk
+    )
+  }
+
+  return Buffer.concat(chunks)
+}
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   console.log('evento recebido')
